4b22111d65f68721061310db7ce3e80e341b1906
Амир Кахаров <amir.kakharov2001@gmail.com>
2023-11-25 04:44:20 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+
+class Program
+{
+    static void Main()
+    {
+        // Количество элементов для вставки
+        int elementsCount = 1000000;
+
+        // Создаем List и LinkedList
+        List<int> list = new List<int>();
+        LinkedList<int> linkedList = new LinkedList<int>();
+
+        // Используем Stopwatch для измерения времени
+        Stopwatch listStopwatch = new Stopwatch();
+        Stopwatch linkedListStopwatch = new Stopwatch();
+
+        // Измеряем время вставки в List
+        listStopwatch.Start();
+        for (int i = 0; i < elementsCount; i++)
+        {
+            list.Add(i);
+        }
+        listStopwatch.Stop();
+
+        // Измеряем время вставки в LinkedList
+        linkedListStopwatch.Start();
+        for (int i = 0; i < elementsCount; i++)
+        {
+            linkedList.AddLast(i);
+        }
+        linkedListStopwatch.Stop();
+
+        // Выводим результаты
+        Console.WriteLine($"Время вставки в List: {listStopwatch.ElapsedMilliseconds} миллисекунд");
+        Console.WriteLine($"Время вставки в LinkedList: {linkedListStopwatch.ElapsedMilliseconds} миллисекунд");
+    }
+}
---
+++ Task2.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+class Task2
+{
+    static void Main()
+    {
+        Console.Write("Введите путь до файла с текстом: ");
+        string filePath = Console.ReadLine();
+
+        if (File.Exists(filePath))
+        {
+            string text = File.ReadAllText(filePath);
+
+            string[] words = text.Split(new[] { ' ', '.', ',', '!', '?' }, StringSplitOptions.RemoveEmptyEntries);
+
+            var wordFrequency = words.GroupBy(word => word)
+                                    .ToDictionary(group => group.Key, group => group.Count());
+
+            var sortedWords = wordFrequency.OrderByDescending(pair => pair.Value);
+
+            int topWordsCount = 10;
+            int count = 0;
+            foreach (var pair in sortedWords)
+            {
+                Console.WriteLine($"{pair.Key}: {pair.Value} раз");
+                count++;
+
+                if (count >= topWordsCount)
+                    break;
+            }
+        }
+        else
+        {
+            Console.WriteLine("Файл не найден.");
+        }
+    }
+}
